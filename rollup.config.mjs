a27deee5b00777157b7578e93683aa933780e9f9
17Sergey <sergeykachaliukdev@gmail.com>
2024-12-30 09:39:26 +0300

    setup: add project main configuration

---
+++ rollup.config.mjs
@@ -0,0 +1,64 @@
+import path from "path";
+
+import { babel } from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import alias from "@rollup/plugin-alias";
+import image from "@rollup/plugin-image";
+
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "lib/index.js",
+        format: "cjs",
+        exports: "named",
+        interop: "auto",
+        sourcemap: true,
+      },
+      {
+        file: "lib/index.es.js",
+        format: "es",
+        interop: "esModule",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+        babelHelpers: "bundled",
+      }),
+      external(),
+      resolve({
+        extensions: [
+          ".mjs",
+          ".js",
+          ".json",
+          ".node",
+          ".jsx",
+          ".tsx",
+          ".ts",
+          ".svg",
+        ],
+      }),
+      commonjs(),
+      image(),
+      alias({
+        entries: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+    external: ["react", "react-dom", "styled-components"],
+  },
+];
+
